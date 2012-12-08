6c82dc0c1d18f5307980e12fb7f6bf07612703bd
Daniel Kvasnicka <daniel.kvasnicka.jr@gmail.com>
2012-12-08 19:20:50 +0100

    Web listener to set production DB path

---
+++ src/main/jvm/net/danielkvasnicka/sympto/setup/SetupCtxListener.java
@@ -0,0 +1,35 @@
+package net.danielkvasnicka.sympto.setup;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.logging.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: daniel
+ * Date: 08.12.12
+ * Time: 17:39
+ */
+@WebListener
+public class SetupCtxListener implements ServletContextListener {
+
+    public static final Logger LOGGER = Logger.getLogger(SetupCtxListener.class.getSimpleName());
+    public static final String BASEX_DBPATH_PRODUCTION = "basex";
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        String openshiftDataDir = System.getenv("OPENSHIFT_DATA_DIR");
+        if (openshiftDataDir != null) {
+            String dbpathAbsolute = openshiftDataDir + BASEX_DBPATH_PRODUCTION;
+            sce.getServletContext().setInitParameter("org.basex.dbpath", dbpathAbsolute);
+            LOGGER.info("Production mode - BaseX dbpath: " + dbpathAbsolute);
+        } else {
+            LOGGER.info("Development mode - BaseX dbpath will remain unchnged.");
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
