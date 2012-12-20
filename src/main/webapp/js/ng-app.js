cfbd55d9a31a82c01643134a9191e8691466d15b
Daniel Kvasnicka <daniel.kvasnicka.jr@gmail.com>
2012-12-20 15:15:40 +0100

    Cleanup

--- src/main/webapp/js/ng-app.js
+++ src/main/webapp/js/ng-app.js
@@ -19,33 +19,37 @@ sympto.config(function($provide, $httpProvider) {
     
     // 500 interceptor
     $httpProvider.responseInterceptors.push(function($q) {
-        return function(promise) {
-            return promise.then(function(response) {
-                return response;
-            }, function(response) {            
-                if (response.status == 500) {
-                    alert(response.data);
-                }    
-                return $q.reject(response);
-            });
-        }
+        return createErrorInterceptor(function(response) {            
+            if (response.status == 500) {
+                alert(response.data);
+            }    
+            return $q.reject(response);
+        });
     });
     
     // 403 interceptor
     $httpProvider.responseInterceptors.push(function($q, $location, $rootScope) {
-        return function(promise) {
-            return promise.then(function(response) {
-                return response;
-            }, function(response) {            
-                if (response.status == 403) {
-                    $location.path('/');
-                    $rootScope.$broadcast("403");
-                }    
-                return $q.reject(response);
-            });
-        }
+        return createErrorInterceptor(function(response) {            
+            if (response.status == 403) {
+                $location.path('/');
+                $rootScope.$broadcast("403");
+            }    
+            return $q.reject(response);
+        });
     });
 });
+
+/**
+ * Utility function that creates error-only interceptor from a template.
+ * Does nothing on 200.
+ */
+function createErrorInterceptor(errorFn) {
+    return function(promise) {
+        return promise.then(function(response) {
+            return response;
+        }, errorFn);
+    }
+}
 
 
 /**
