fb91616d4954507295fa7cdc67f5b5fe6c27f1b4
Daniel Kvasnicka <daniel.kvasnicka.jr@gmail.com>
2012-12-26 20:55:28 +0100

    Supporting cycles with < 7 measurements

--- src/main/jvm/sympto.cljs
+++ src/main/jvm/sympto.cljs
@@ -3,13 +3,15 @@
         [clojure.zip :as z]))
 
 (defn ^:export detectInfertilePeriod
-  "Detects infertile period based on credible temperature rise occurence"
+  "Detects infertile period based on credible temperature rise occurence.
+  Returns the position of the first temp rise or -1 if none."
   [measurements]
   (let [startLoc
     ; find the first loc that has 6 preceding siblings
     (loop [ptr (clojure.zip/down (clojure.zip/seq-zip
                                    (map #(nth % 1) measurements)))]
       (if (nil? ptr)
+        ; nothing found
         nil
         (if (= 6 (count (clojure.zip/lefts ptr)))
           ptr
@@ -30,9 +32,12 @@
               ; 6 prec. s. are lower
               (every? true? (map #(< % (clojure.zip/node l)) precSix))
               ; the 2 right siblings are at least 0.2 higher than max(prec6)
-              ; dirty hack to make Clojure float compath with ClojureScript
+              ; dirty hack to make Clojure float compat with ClojureScript
               (every? true? (map #(>= (- % precSixMax) 0.19999)
-                              (take 2 (clojure.zip/rights l))))))
+                              (let [rghts (take 2 (clojure.zip/rights l))]
+                                (if (empty? rghts)
+                                  '(-1)
+                                  rghts))))))
           ; return the position...
           (count (clojure.zip/lefts l))
           ; ...or move right and check again
@@ -40,15 +45,18 @@
       )))
 
 ; --------------- TESTS
-;(deftest testDetectInfertilePeriod
-;    (is (= 6 (detectInfertilePeriod
-;               [[0 36.5] [0 36.3] [0 36.5] [0 36.4] [0 36.6] [0 36.5] [0 36.7] [0 36.8] [0 36.8]])))
-;
-;    (is (= 7 (detectInfertilePeriod
-;               [[0 37] [0 36.5] [0 36.3] [0 36.5] [0 36.4] [0 36.6] [0 36.5] [0 36.7] [0 36.8] [0 36.8]])))
-;
-;    (is (= -1 (detectInfertilePeriod
-;              [[0 35] [0 36]]))))
-;
-;
-;(run-tests 'cljsympto.core)
+(deftest testDetectInfertilePeriod
+    (is (= 6 (detectInfertilePeriod
+               [[0 36.5] [0 36.3] [0 36.5] [0 36.4] [0 36.6] [0 36.5] [0 36.7] [0 36.8] [0 36.8]])))
+
+    (is (= 7 (detectInfertilePeriod
+               [[0 37] [0 36.5] [0 36.3] [0 36.5] [0 36.4] [0 36.6] [0 36.5] [0 36.7] [0 36.8] [0 36.8]])))
+
+    (is (= 7 (detectInfertilePeriod
+               [[0 37] [0 36.5] [0 36.3] [0 36.5] [0 36.4] [0 36.6] [0 36.5] [0 36.7] [0 36.6] [0 36.8]])))
+
+    (is (= -1 (detectInfertilePeriod
+              [[0 35] [0 36]]))))
+
+
+(run-tests 'cljsympto.core)
